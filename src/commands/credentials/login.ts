b9ec28f7cdabcec573d472d94d2024df218a0fad
Aidan Cunniffe <acunniffe@gmail.com>
2019-03-30 10:52:42 -0400

    Updated login call

--- src/commands/credentials/login.ts
+++ src/commands/credentials/login.ts
@@ -1,17 +1,22 @@
-import {Command} from '@oclif/command'
+import {Command, flags} from '@oclif/command'
 import {cli} from 'cli-ux'
 import { parseOpticYaml, readOpticYaml } from '../../common/config'
 import {Credentials} from '../../common/credentials'
 import { TokenListenerService } from '../../services/optic-authentication/token-listener'
 
-export default class CredentialsLogin extends Command {
+export default class Login extends Command {
   static description = 'login to your account from the CLI'
 
-  static flags = {}
+  static flags = {
+    "host": flags.string({required: false})
+  }
 
   static args = []
 
   async run() {
+
+
+    const {flags} = this.parse(Login)
 
     let yamlContents;
     try{
@@ -25,7 +30,7 @@ export default class CredentialsLogin extends Command {
 
     const tokenService = new TokenListenerService()
     const {tokenPromise, responsePort} = await tokenService.waitForToken()
-    cli.open(`${baseUrl}/authorize-cli/`+responsePort)
+    cli.open(`${flags.host || baseUrl}/authorize-cli/`+responsePort)
 
     const token = await tokenPromise
     await new Credentials().set(token.trim())
