7770b5b541227b9d4c18b43fb592432e466c979f
Dev Doshi <dev@devdoshi.com>
2019-03-30 15:05:40 -0400

    updated cli authorization path

--- src/commands/credentials/login.ts
+++ src/commands/credentials/login.ts
@@ -1,28 +1,30 @@
 import {Command, flags} from '@oclif/command'
 import {cli} from 'cli-ux'
-import { parseOpticYaml, readOpticYaml } from '../../common/config'
+import {parseOpticYaml, readOpticYaml} from '../../common/config'
 import {Credentials} from '../../common/credentials'
-import { TokenListenerService } from '../../services/optic-authentication/token-listener'
+import {TokenListenerService} from '../../services/optic-authentication/token-listener'
 
 export default class Login extends Command {
   static description = 'login to your account from the CLI'
 
   static flags = {
-    "host": flags.string({required: false})
+    'host': flags.string({required: false})
   }
 
   static args = []
 
   async run() {
 
-
     const {flags} = this.parse(Login)
 
-    let yamlContents;
-    try{
+    let yamlContents
+    try {
       yamlContents = readOpticYaml()
     } catch {
-      yamlContents = JSON.stringify({strategy: {type: 'logging', commandToRun: 'npm'},  api: {id: 'none', version: 'abc', paths: []}})
+      yamlContents = JSON.stringify({
+        strategy: {type: 'logging', commandToRun: 'npm'},
+        api: {id: 'none', version: 'abc', paths: []}
+      })
     }
 
     const config = parseOpticYaml(yamlContents)
@@ -30,13 +32,13 @@ export default class Login extends Command {
 
     const tokenService = new TokenListenerService()
     const {tokenPromise, responsePort} = await tokenService.waitForToken()
-    cli.open(`${flags.host || baseUrl}/authorize-cli/`+responsePort)
+    cli.open(`${flags.host || baseUrl}/clis/${responsePort}/authorization`)
 
     const token = await tokenPromise
     await new Credentials().set(token.trim())
 
     tokenService.stop()
 
-    this.log("You've been logged in!")
+    this.log('You\'ve been logged in!')
   }
 }
